a47d133a599b2c16f62730097e6ce044d9ed0996
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-07-03 17:39:57 -0400

    Add basic word swap

--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,11 @@
+var arrNouns = ['node', 'atom', 'brick', 'react', 'mouse', 'store', 'tree'];
+
+function fillWords() {
+    
+}
+
+fillWords();
+
 var generator = new Vue({
     el: '#generated-name',
     data: {
@@ -5,7 +13,7 @@ var generator = new Vue({
     },
     methods: {
         randomWord: function() {
-            
+            this.name = arrNouns[Math.floor(Math.random() * arrNouns.length)];
         }
     }
 })
