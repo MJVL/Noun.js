7d993f2c11331c6cc2ea21dd617a9d22eb4b4faa
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-07-05 16:49:07 -0400

    Refactor name -> package

--- js/utility.js
+++ js/utility.js
@@ -1,19 +1,19 @@
 var generator = new Vue({
     el: '#generated-name',
     data: {
-        name: DEFAULT,
+        package: DEFAULT,
         exists: true,
-        result: 'This repository does exist.'
+        result: 'This package does exist.'
     },
     methods: {
         randomWord: function() {
-            this.name = NOUNS[Math.floor(Math.random() * NOUNS.length)];
-            this.name = this.name.charAt(0).toUpperCase() + this.name.substr(1) + EXTENSION;
+            this.package = NOUNS[Math.floor(Math.random() * NOUNS.length)];
+            this.package = this.package.charAt(0).toUpperCase() + this.package.substr(1) + EXTENSION;
             this.checkExistence();
         },
         checkExistence: function() {
             //this.exists =
-            this.result = 'This repository does' + ((this.exists) ? '' : "n't") + ' exist.'; 
+            this.result = 'This package does' + ((this.exists) ? '' : "n't") + ' exist.'; 
         }
     }
 })
@@ -22,7 +22,7 @@ var tray = new Vue({
     el: '#icon-tray',
     methods: {
         searchForName: function() {
-            window.open('https://www.google.com/search?q=' + generator.name);
+            window.open('https://www.google.com/search?q=' + generator.package);
         },
         travelToRepository: function() {
             window.open('https://github.com/MJVL/Noun.js');
