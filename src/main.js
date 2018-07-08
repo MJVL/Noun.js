c7ef0f5b8a58df44e9d1a0ca0e2569724a547429
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-07-07 20:40:06 -0600

    Add comment and semicolons

--- src/main.js
+++ src/main.js
@@ -63,10 +63,11 @@ var generator = new Vue({
 Vue.component('github-button', {
     template: '<img src="src/assets/github.png" id="repository" v-on:click="travelToRepository"/>',
     methods: {
+        // Won't work for users until repository is public
         travelToRepository: function() {
             window.open('https://github.com/MJVL/Noun.js');
         }
     }
-})
+});
 
-new Vue({ el: '#icon-tray' })
+new Vue({ el: '#icon-tray' });
